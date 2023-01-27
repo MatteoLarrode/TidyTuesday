ee8ef6cf983c6dc692509dc441b757929a132447
MatteoLarrode <matteo.larrode@orange.fr>
2023-01-27 01:24:25 -0500

    found idea week 1

--- 2023/Week2/week2.R
+++ 2023/Week2/week2.R
@@ -16,6 +16,11 @@ library("tidytuesdayR") #get data
 feederwatch_raw <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-10/PFW_2021_public.csv')
 
 
+#IDEA: keep migratory birds
+#animate map with sightings for those birds
+
+
+
 
 # visualization ----------------------------------------------------------
 
@@ -24,3 +29,5 @@ feederwatch_raw <- readr::read_csv('https://raw.githubusercontent.com/rfordatasc
 #The maps are surface maps interpolated from the locations as inverse distance weighted averages using an influence radius of 100 kilometers and a power of -2 decay of influence. 
 #In other words, each point on the surface maps is the mean of all values with a radius of 100 kilometers weighted by the inverse of the square root of the distance to each point. 
 
+
+#animate using gganimate
