aa5d8bfd03fc35a8048c6e82b12c04f81942496a
MatteoLarrode <matteo.larrode@orange.fr>
2023-01-27 00:52:59 -0500

    loaded data

--- 2023/Week2/week2.R
+++ 2023/Week2/week2.R
@@ -4,3 +4,23 @@
 # THEME: "Project FeederWatch"
 # ------------------------------------------------------------------------
 
+# load packages ----------------------------------------------------------
+
+library("tidyverse")
+library("tidytuesdayR") #get data
+
+
+# data wrangling ----------------------------------------------------------
+
+#load data
+feederwatch_raw <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-10/PFW_2021_public.csv')
+
+
+
+# visualization ----------------------------------------------------------
+
+
+
+#The maps are surface maps interpolated from the locations as inverse distance weighted averages using an influence radius of 100 kilometers and a power of -2 decay of influence. 
+#In other words, each point on the surface maps is the mean of all values with a radius of 100 kilometers weighted by the inverse of the square root of the distance to each point. 
+
