1ee93175d6c78ae250e3f0dcf1fa62e80a1e3699
MatteoLarrode <matteo.larrode@orange.fr>
2023-02-07 23:41:42 -0500

    ridgeline #1 (still cycles to do)

--- 2023/Week3/week3.R
+++ 2023/Week3/week3.R
@@ -18,12 +18,13 @@ library(forcats)
 tuesdata <- tidytuesdayR::tt_load('2023-01-17')
 arthistory <- tuesdata$artists
 
-selected_years <- c("1926", "1936", "1948", "1959", "1970", "1977", "1986", "1995", "2001", "2007", "2013", "2020")
+selected_years <- c("1926", "1936", "1948", "1959", "1970", "1975", "1991", "1995", "2005", "2009", "2013", "2020")
+breaks <- c("2020", "2009", "1995", "1975", "1959","1926")
 
 # data wrangling ----------------------------------------------------------
 
 #pre-selection of variables
-arthistory <- arthistory %>%
+arthistory_df <- arthistory %>%
   select(-artist_nationality, -artist_nationality_other, -artist_ethnicity, -artist_unique_id) %>%
   filter(artist_gender != "N/A" & artist_race != "N/A") %>% #na.omit() not working because of the "/"
   filter(space_ratio_per_page_total >= 0 & space_ratio_per_page_total <= 1) %>%
@@ -32,56 +33,60 @@ arthistory <- arthistory %>%
          genderFct = as.factor(artist_gender)) #change some variables to factor for fill aes()
 
 #further variable selection & reduce number of years displayed (22 -> 15)
-arthistory_df <- arthistory %>%
-  select(yearFct, genderFct, artist_race_nwi, space_ratio_per_page_total)%>%
+arthistory_final <- arthistory_df %>%
+  select(yearFct, genderFct, artist_race_nwi, space_ratio_per_page_total) %>%
   filter(yearFct %in% selected_years)
 
 
-
 # visualization----------------------------------------------------------
-#INTERSECTIONAL ANALYSIS
-  #most viz focus on either gender or ethnicity
-  #consider both as interrelated
 
-#ridgeline plot!
-  #x axis: measure of importance in history books, separated by gender
-  #y axis: evolution over time
-  #add ethnicity with facet?
+#1) Gender representation as page ratio ------
 
-ridge_plot <- ggplot(arthistory_df, 
+  #!! density grouped by gender so does NOT show differences in total representations genders
+
+ridge_plot <- ggplot(arthistory_final, 
                      aes(y = yearFct))+
-  scale_y_discrete(breaks = c("2020", "2007", "1995", "1986", "1970", "1948", "1926"))+
-  
+  scale_y_discrete(breaks = breaks)+
   geom_density_ridges(aes(x = space_ratio_per_page_total,
                           fill = genderFct), # fill created later
                       rel_min_height = 0.07,
-                      alpha = .8,
+                      alpha = .7,
                       color = "white",
-                      scale = 1.5,
+                      scale = 2,
                       quantile_lines = TRUE, 
                       quantiles = 2,
                       from = 0, to = 1)+
-  
   scale_fill_cyclical(
     breaks = c("Male", "Female"),
     values = c("#ff0000", "#0000ff", "#ff8080", "#8080ff"),
     labels = c(`Male` = "Male Artists", `Female` = "Female Artists"),
-    name = "Gender", guide = "legend"
-  ) +
-  labs(
-    x = "Page space ratio (area of both text and figure of the artist / area of a page)",
-    y = "Year",
-    title = "Gender representation in popular art history textbooks",
-    subtitle = "Differences in space taken by artists on the pages of Janson’s History of Art and Gardner’s Art Through the Ages",
-    caption = "Matteo Larrode (@matteoStats) | Source: Lemus S, Stam H (2022). arthistory: Art History Textbook Data")+
+    name = "Gender", guide = "legend") +
+  labs(x = "Page space ratio (area of both text and figure of the artist / area of a page)",
+       y = NULL,
+       title = "Gender representation in popular art history textbooks",
+       subtitle = "Differences in space taken by artists on the pages of Janson’s History of Art and Gardner’s Art Through the Ages",
+       caption = "Matteo Larrode (@matteoStats) | Source: Lemus S, Stam H (2022). arthistory: Art History Textbook Data")+
   theme_ridges(grid = FALSE, center_axis_labels = TRUE)+
   theme(text=element_text(family="Helvetica"),
+        plot.margin = margin(l = 30, t = 15, b = 15),
         plot.title=element_text(hjust=0.5, face="bold", size=17),
         plot.subtitle=element_text(hjust=0.5, size=13, margin=margin(t=15, b = 15), face="italic"),
-        plot.caption=element_text(size=10, hjust=0.95, margin=margin(t = 15, b=12)))
-  
+        plot.caption=element_text(hjust=1, size=10, margin=margin(t = 30, b=15)))
+
 
 ridge_plot
+
+
+#2) Total representation ------
+
+#INTERSECTIONAL ANALYSIS
+  #most viz focus on either gender or ethnicity
+  #consider both as interrelated
+
+#choice:
+  #total appearances in both textbooks (or either)
+  #total exhibitions in Moma and / or Whitney
+
 
 
 
