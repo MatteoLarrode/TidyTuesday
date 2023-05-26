66b23b49b2fbc2415026e8d190d8b9f880124454
MatteoLarrode <matteo.larrode@orange.fr>
2023-05-26 15:20:44 +0200

    finished week3

--- 2023/Week3/README.md
+++ 2023/Week3/README.md
@@ -1,2 +1,3 @@
 WEEK 3 - ART HISTORY
 ================
+![Art History](gender_page_ratio.png)
--- 2023/Week3/week3.R
+++ 2023/Week3/week3.R
@@ -18,7 +18,7 @@ library(forcats)
 tuesdata <- tidytuesdayR::tt_load('2023-01-17')
 arthistory <- tuesdata$artists
 
-selected_years <- c("1926", "1936", "1948", "1959", "1970", "1975", "1991", "1995", "2005", "2009", "2013", "2020")
+selected_years <- c("1936", "1948", "1959", "1970", "1975", "1991", "1995", "2005", "2009", "2013", "2020")
 
 # data wrangling ----------------------------------------------------------
 
@@ -57,11 +57,11 @@ ridge_plot <- ggplot(arthistory_final,
     breaks = c("Male", "Female"),
     values = c("#ff0000", "#0000ff", "#ff8080", "#8080ff"),
     labels = c(`Male` = "Male Artists", `Female` = "Female Artists"),
-    name = "Gender", guide = "legend") +
-  labs(x = "Page space ratio (area of both text and figure of the artist / area of a page)",
+    name = NULL , guide = "legend") +
+  labs(x = "Page Space Ratio (area of text & figure of the artist / area of page)",
        y = NULL,
        title = "Gender representation in popular art history textbooks",
-       subtitle = "Differences in space taken by artists on the pages of Janson’s History of Art and Gardner’s Art Through the Ages",
+       subtitle = "Differences in space taken by artists on the pages of the different editions of \n Janson’s History of Art and Gardner’s Art Through the Ages",
        caption = "Matteo Larrode (@matteoStats) | Source: Lemus S, Stam H (2022). arthistory: Art History Textbook Data")+
   theme_ridges(grid = FALSE, center_axis_labels = TRUE)+
   theme(text=element_text(family="Roboto", color = "#22211d"),
@@ -69,12 +69,17 @@ ridge_plot <- ggplot(arthistory_final,
         panel.background = element_rect(fill = "#f5f5f2", color = NA), 
         legend.background = element_rect(fill = "#f5f5f2", color = NA),
         plot.margin = margin(l = 30, t = 15, b = 15, r = 15),
-        plot.title=element_text(hjust=0.5, face="bold", size=17,  color = "#4e4d47"),
-        plot.subtitle=element_text(hjust=0.5, size=13, margin=margin(t=12, b = 15), color = "#4e4d47"),
-        plot.caption=element_text(hjust=0.9, size=9, margin=margin(t = 30, b=15), color = "#4e4d47"),
-        legend.key.height= unit(0.5, 'cm'),
-        legend.key.width= unit(0.5, 'cm'),
-        legend.position = c(0.9, 0.5))
+        plot.title=element_text(hjust=0.5, face="bold", size=25,  color = "#4e4d47"),
+        plot.subtitle=element_text(hjust=0.5, size=19, margin=margin(t=12, b = 15), color = "#4e4d47"),
+        plot.caption=element_text(hjust=0.9, size=13, margin=margin(t = 30, b=15), color = "#4e4d47"),
+        legend.key.height= unit(1, 'cm'),
+        legend.key.width= unit(1, 'cm'),
+        legend.position = c(0.87, 0.5),
+        legend.text = element_text(size = 13),
+        axis.text.y = element_text(hjust = 1))
 
 
 ridge_plot
+
+
+ggsave("2023/Week3/gender_page_ratio.png", height = 9, width = 15)
